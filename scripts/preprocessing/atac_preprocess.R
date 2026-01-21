21e7edd74c2804a015ce284e1a1ce2e3ff987433
Madeleine Peng <madeleine@Stars-Nunes.nyspi.local>
2026-01-21 12:44:35 -0500

    update atac preprocess

--- scripts/preprocessing/atac_preprocess.R
+++ scripts/preprocessing/atac_preprocess.R
@@ -1,3 +1,5 @@
+# ATAC preprocessing
+
 library(GenomicRanges)
 library(Seurat)
 library(Signac)
@@ -11,7 +13,7 @@ library(harmony)
 # Genomic annotations 
 annotations <- GetGRangesFromEnsDb(ensdb = EnsDb.Hsapiens.v86)
 seqlevelsStyle(annotations) <- "UCSC"
-genome(annotations) <- "hg38"
+#genome(annotations) <- "hg38"
 
 
 ############################# HELPER FUNCTIONS #################################
@@ -29,6 +31,7 @@ create_peak_set <- function(list, method = c('macs', 'cellranger')) {
                        macs2.path = MACS_2_PATH, additional.args= '--min-len 100 --max-gap 50')
     
   } else if (method == 'cellranger') {
+    
     peaks <- lapply(list, function(x) {
       path <- get_raw_path_multiome(x)
       peak <- read.table(
@@ -54,31 +57,23 @@ create_peak_set <- function(list, method = c('macs', 'cellranger')) {
 
 # Creates seurat object from id and unified peak set
 # Make sure to change paths accordingly
-create_atac_srat <- function(id, peak_set, rna_meta) {
+create_atac_srat <- function(id, peak_set) {
   
-  if (file.exists(paste0(ATAC_SAVED_RDS_PATH, id, "_atac_macs_peaks.rds"))) {
-    srat <- LoadSeuratRds(paste0(ATAC_SAVED_RDS_PATH, id, "_atac_macs_peaks.rds"))
+  if (file.exists(paste0(ATAC_PROCESSED_RDS_PATH, id, "_atac_combined_peaks.rds"))) {
+    srat <- LoadSeuratRds(paste0(ATAC_PROCESSED_RDS_PATH, id, "_atac_combined_peaks.rds"))
     
   } else {
     
     print(paste0("Reading in metadata for ", id))
+    
     # Read in metadata
+    metadata_path <- paste0(get_raw_path_multiome(id), "/per_barcode_metrics.csv")
     meta <- read.table(
-      file = paste0(RAW_DATA_PATH, id, "/per_barcode_metrics.csv"),
+      file = metadata_path,
       stringsAsFactors = FALSE,
       sep = ",",
       header = TRUE,
       row.names = 1)
-    
-    # Filter based on RNA QC
-    meta <- rna_meta %>% dplyr::filter(sample == id)
-    meta <- meta[rna_sample_meta$gex_barcode,]
-    
-    # if (nrow(meta) < 1) {
-    #   individual_srat <- LoadSeuratRds(paste0(RNA_PROCESSED_RDS_PATH, id, "_rds_scDbl.rds"))
-    #   meta <- individual_srat@meta.data %>% as.data.frame()
-    #   rownames(meta) <- meta$gex_barcode
-    # }
     
     # Create Fragment object
     print(paste0("Creating Fragments object for ", id))
@@ -102,28 +97,32 @@ create_atac_srat <- function(id, peak_set, rna_meta) {
       fragments = fragments,
       annotation = annotations)
     
+    # Create Seurat Object from chromatin assay
     srat <- CreateSeuratObject(
       sample_assay, 
       assay = "ATAC", 
       project = id, 
       meta.data=meta)
     
+    
+    # QC metrics filter
     srat <- NucleosomeSignal(srat) #Low NS = high chromatin accessibility
     srat <- TSSEnrichment(srat, fast = FALSE) #High TSSE = high chromatin accessibility
     srat$pct_reads_in_peaks <- srat$atac_peak_region_fragments / srat$atac_fragments * 100
     srat$blacklist_fraction <- FractionCountsInRegion(srat, assay = 'ATAC', regions = blacklist_hg38_unified)
     srat$sample = id
     
-    srat <- subset(srat, subset = nCount_ATAC > 2e2 &
+    srat_filt <- subset(srat, subset = nCount_ATAC > 2e2 &
                      nCount_ATAC < 1e5 &
                      nucleosome_signal < 3 &
                      TSS.enrichment > 1)
     
-    saveRDS(srat, paste0(ATAC_SAVED_RDS_PATH, id, "_atac_macs_peaks.rds"))
+    # save filtered object
+    saveRDS(srat_filt, paste0(ATAC_SAVED_RDS_PATH, id, "_atac_combined_peaks.rds"))
     
   }
   print(paste0("Done with sample", id))
-  return(srat)
+  return(srat_filt)
   
 }
 ################################################################################
@@ -138,13 +137,12 @@ rna_meta$gex_barcode <- substr(rownames(rna_meta), 7,24)
 combined_peaks <- create_peak_set(ids, method = 'cellranger')
 
 atac_srats <- lapply(ids, function(id) {
-  srats <- create_atac_srat(id, called_peaks, rna_meta = rna_meta)
+  srats <- create_atac_srat(id, combined_peaks)
   return(srats)
 })
 
-gc()
 
-# Merge the ATAC seurat_object
+# If working with the macs3 peak set, can merge the ATAC seurat_object here
 atac_merge <- merge(
   x = atac_srats[[1]],
   y = atac_srats[2:length(atac_srats)],
@@ -154,9 +152,9 @@ atac_merge <- merge(
 # Save the merged Atac
 saveRDS(atac_merge, paste0(MERGED_RDS_PATH, "merged_atac_macs3_iter9.rds"))
 
+# Free up space
 rm(atac_srats)
 gc()
-
 options(future.globals.maxSize = 1000 * 1024^2)
 
 # Feature selection, dim reduction, clustering etc
@@ -168,11 +166,10 @@ atac_merge <- FindNeighbors(atac_merge, dims=2:50, reduction='lsi')
 atac_merge <- FindClusters(atac_merge, algorithm=3)
 
 # Plot pre-integration
-pdf(paste0(MERGED_PLOTS_PATH, "atac_pre_integration.pdf"))
+pdf(paste0(PLOTS_PATH, "atac_merged_pre_integration.pdf"))
 plt <- DimPlot(atac_merge, group.by = 'sample', pt.size = 0.5, reduction = 'umap.atac', raster = F)
 print(plt)
 dev.off()
-
 
 # Integrate with harmony
 atac_merge <- RunHarmony(atac_merge, group.by.vars = c('sample', 'ID4', 'batch'), reduction = 'lsi', dims.use = 2:50,
@@ -188,7 +185,7 @@ atac_merge <- RunUMAP(atac_merge, reduction = "harmony.atac", dims = 1:49,
 
 
 # Plot clusters post integration
-pdf(paste0(MERGED_PLOTS_PATH, "atac_harmony.pdf"), width = 8, height = 6)
+pdf(paste0(PLOTS_PATH, "atac_harmony.pdf"), width = 8, height = 6)
 p2<- DimPlot(atac_merge, reduction = "umap.atac.harmony", group.by =c('sample', 'atac_harmony_clusters_0.8'), 
              label = TRUE,
              label.size = 3, repel = TRUE, raster = F, ncol=2) +
