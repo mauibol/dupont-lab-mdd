da322072af2d8d4500dff4328bf86c24251d6acd
madeleinepeng <peng.mad@gmail.com>
2026-01-22 13:35:20 -0500

    add tobias config for neurogenic clusters

--- scripts/analysis/tobias/configure_tobias.R
+++ scripts/analysis/tobias/configure_tobias.R
@@ -19,7 +19,7 @@ atac_merge <- LoadSeuratRds(paste0(MERGED_RDS_PATH, "merged_atac_macs3.rds"))
 meta <- read.csv("Multiome_process/meta/current_working_multiome_meta (1).csv", row.names=1)
 atac_merge@meta.data <- meta
 
-# Run call peaks on a subset of cell types
+# Run call peaks on a subset of cell types 
 cellTypes <- c('ExN.1', 'InN.5', 'GC.1')
 grs <- CallPeaks(atac_merge, group.by = 'cell_annotation_leiden', idents=cellTypes, macs2.path = MACS_2_PATH, combine.peaks=FALSE)
 
@@ -56,3 +56,43 @@ for (c in cellTypes) {
     
   }
 }
+
+
+# Same thing but on neurogenic clusters for comparison with each other
+cellTypes <- c('NSC','INP','NB','ImGC.2','ImGC.1','mGC')
+grs <- CallPeaks(atac_merge, group.by = 'neuro_annotations', idents=cellTypes, macs2.path = MACS_2_PATH, combine.peaks=TRUE)
+
+# Save bed files
+setwd('/groups/mb928_gp/mp4486/tobias')
+
+for (i in 1:length(grs)) {
+  gr <- grs[[i]]
+  gr_df <- as.data.frame(gr)[,c("seqnames", "start", "end")]
+  colnames(gr_df) <- c("Chromosome", "Start", "End")
+  
+  path = paste0("neurogenesis/peaks/trajectory_ALL.bed")
+  write.table(gr_df, file = path, sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+}
+
+for (c in cellTypes) {
+  for (s in split) {
+    cells <- s$annotations_broad[s$annotations_broad == c]
+    if (length(cells) == 0) {
+      print(paste('There are 0 cells in', c, "cluster of sample", s$sample[1], "skipping this sample..."))
+      next
+    }
+    print(paste('There are', length(cells), 'cells in', c, "cluster of sample", s$sample[1]))
+    print('Reading barcodes')
+    srat <- subset(s, subset = annotations_broad == c)
+    print(dim(srat))
+    barcodes <- as.list(srat$gex_barcode)
+    print(length(barcodes))
+    barcodes <- lapply(barcodes, function(x) paste0("CB:Z:", x))
+    file_path <- paste0("CURRENT/barcodes/", srat$sample[1], "_", gsub("[ /]", "_", c), ".txt")
+    if (!file.exists(file_path)) {
+      cat(unlist(barcodes), file = file_path, sep = "\n")
+    }
+    
+  }
+}
+
